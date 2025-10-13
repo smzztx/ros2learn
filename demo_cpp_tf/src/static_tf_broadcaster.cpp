982b811b104d99be4332e304773b0eb3973a0ba8
tangxiang <tangxiang007@gmail.com>
2025-10-13 18:11:42 +0800

    add static_tf_broadcaster.cpp.

---
+++ demo_cpp_tf/src/static_tf_broadcaster.cpp
@@ -0,0 +1,5 @@
+#include <memory>
+#include <rclcpp/rclcpp.hpp>
+#include <tf2_ros/static_transform_broadcaster.h>
+#include <geometry_msgs/msg/transform_stamped.hpp>
+#include <tf2/LinearMath/Quaternion.h>
